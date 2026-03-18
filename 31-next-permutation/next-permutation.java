639707d0e61e4171d912b0a96de17e2e0b4d337f
JATIN MALLARAPU <109145951+jatinmallarapu@users.noreply.github.com>
2026-03-17 23:12:00 -0500

    Time: 0 ms (100.00%) | Memory: 44.7 MB (64.52%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int index=-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1){
+            reverse(nums,0,nums.length-1);
+            return;
+        }
+        for(int i=nums.length-1;i>index;i--){
+            if(nums[i]>nums[index]){
+                int temp=nums[i];
+                nums[i]=nums[index];
+                nums[index]=temp;
+                break;
+            }
+        }
+        reverse(nums,index+1,nums.length-1);
+    }
+    public void reverse(int[] arr, int left, int right){
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+
+    }
+}
