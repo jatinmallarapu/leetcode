92ff48c95718fe0a3d8f0d308ae3cedbf6955064
JATIN M <109145951+jatinmallarapu@users.noreply.github.com>
2026-04-15 02:12:22 -0500

    Time: 0 ms (100.00%) | Memory: 44.5 MB (86.19%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,34 +1,45 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int index=-1;
-        for(int i=nums.length-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                index=i;
+        for(int i=nums.length-1;i>0;i--){
+            if(nums[i-1]<nums[i]){
+                index=i-1;
                 break;
             }
         }
-        if(index==-1){
-            reverse(nums,0,nums.length-1);
-            return;
-        }
-        for(int i=nums.length-1;i>index;i--){
-            if(nums[i]>nums[index]){
-                int temp=nums[i];
-                nums[i]=nums[index];
-                nums[index]=temp;
+        if(index!=-1){
+        //System.out.println(index);
+        int target=nums[index];
+        int swapIndex=index;
+        for(int i=nums.length-1;i>=index+1;i--){
+            if(nums[i]>target){
+                swapIndex=i;
                 break;
             }
         }
-        reverse(nums,index+1,nums.length-1);
-    }
-    public void reverse(int[] arr, int left, int right){
-        while(left<right){
-            int temp=arr[left];
-            arr[left]=arr[right];
-            arr[right]=temp;
-            left++;
-            right--;
+        swap(nums, index, swapIndex);
+        reverse(nums, index+1, nums.length-1);
+        
         }
-
+        else{
+            reverse(nums, 0, nums.length-1);
+        }
+        
+        //System.out.println(Arrays.toString(nums));
     }
-}
+    
+    void reverse(int[] arr, int i, int j){
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+    void swap(int[] arr, int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    }
